55eeb59b1ddf4e2bbb37a4053b4e2fcce75f38ee
Maria Victória <mavicmiranda99@gmail.com>
2022-07-05 20:47:37 -0300

    commit-3

--- avasus/models.py
+++ avasus/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Avasus(models.Model):
+    nome_curso = models.CharField(max_length=200)
+    descricao = models.TextField()
+    objetivo = models.TextField(null=True)
+    idade = models.IntegerField()
+    ch = models.IntegerField(null=True)
+    data_curso = models.DateTimeField(default=datetime.now, blank=True)
+    modulos = models.IntegerField(null=True)
--- avasus/views.py
+++ avasus/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from .models import Avasus
 
 def index(request):
-    return render(request,'index.html')
+    avasus = Avasus.objects.all()
+
+    dados = {
+        'avasus' : avasus
+    }
+
+    return render(request,'index.html',dados)
 
 def avasus(request):
     return render(request,'avasus.html')
